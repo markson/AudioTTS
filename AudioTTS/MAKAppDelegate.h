bd09167aa793041d51360fd29152000833f85180
Lin <lzh130@uclive.ac.nz>
2013-09-20 14:53:01 +1200

    Initial Commit

---
+++ AudioTTS/MAKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MAKAppDelegate.h
+//  AudioTTS
+//
+//  Created by Linark on 9/20/13.
+//  Copyright (c) 2013 Linark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MAKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AudioTTS/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AudioTTS
+//
+//  Created by Linark on 9/20/13.
+//  Copyright (c) 2013 Linark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MAKAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MAKAppDelegate class]));
+    }
+}
